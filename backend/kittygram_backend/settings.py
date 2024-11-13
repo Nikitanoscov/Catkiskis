a7d7e6c36cd1092c9118ee18db237654316b239e
Nikita Noskov <nik030205@mail.ru>
2024-11-13 21:36:43 +0300

    Update

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,13 +2,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.161.89','kittygarmmydns.zapto.org']
+ALLOWED_HOSTS = os.getenv('HOSTS_PROJECT', '*').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
